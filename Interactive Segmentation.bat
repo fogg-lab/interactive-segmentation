a738c846ee0bc56d0e857c1c9866f5bfc86cc495
Noah <wigginno@oregonstate.edu>
2022-11-21 01:26:08 -0500

    Installer and launcher scripts for windows

--- Interactive Segmentation.bat
+++ Interactive Segmentation.bat
@@ -1,57 +1,10 @@
-echo --------------------------------------------------------------------------------
-echo Interactive Segmentation App Launcher
-echo --------------------------------------------------------------------------------
+@ECHO OFF
+SETLOCAL
 
-SET CondaDir=%cd%\conda
-
-if NOT exist conda\ (
-    echo Installing Miniconda locally in the project folder...
-    echo --------------------------------------------------------------------------------
-    SET Target=Miniconda3-latest-Windows-x86_64.exe
-    echo   downloading %target%...
-    curl.exe --output %Target% --url https://repo.anaconda.com/miniconda/%Target%
-    mkdir %CondaDir%
-    echo   installing Miniconda into %CondaDir%\...
-    .\%Target% /S /D=%CondaDir%
-    DEL .\%Target%
-)
-
-SET CondaExecPath=%CondaDir%\condabin\conda.bat
-SET CondaEnvsDir=%cd%\.conda
-SET CondaEnvDir=%CondaEnvsDir%\iseg
-
-echo Activating conda environment (if it exists)...
-SET CondaEnvActivateCmd=%CondaExecPath% activate %CondaEnvDir%
-
-echo debug_1
-:try_activate_env
-echo debug try_activate_env
-SET EnvActivationFailed=0
-%CondaEnvActivateCmd% | find "Could not find conda environment" > nul && SET EnvActivationFailed=1
-EXIT /B 0
-
-echo debug_2
-CALL :try_activate_env
-
-if %EnvActivationFailed% == 1 (
-    echo Creating conda environment...
-    echo --------------------------------------------------------------------------------
-    %CondaExecPath% config --set remote_max_retries 3
-    %CondaExecPath% install -y -c conda-forge mamba
-    %CondaDir%\condabin\mamba.bat env create -p %CondaEnvsDir%\iseg -f .\environment.yml python=3.10
-    echo Activating conda environment...
-    echo debug_3
-    CALL :try_activate_env
-)
-
-if %EnvActivationFailed% == 1 (
-    echo Failed to activate conda environment!
-    EXIT /B 1
-)
-
-echo Activated conda environment.
+echo Activating conda environment...
+%CondaExecPath% activate %CondaEnvDir%
 
 echo Launching Interactive Segmentation App...
 python iseg.py
 
-read-host "Press ENTER to continue..."
+PAUSE
---
+++ iseg-windows-installer.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+SETLOCAL
+
+echo --------------------------------------------------------------------------------
+echo Interactive Segmentation App Launcher
+echo --------------------------------------------------------------------------------
+
+SET CondaDir=%cd%\conda
+
+if NOT exist conda\ (
+    echo Installing Miniconda locally in the project folder...
+    echo --------------------------------------------------------------------------------
+    SET Target=Miniconda3-latest-Windows-x86_64.exe
+    echo Downloading %Target%...
+    curl.exe --output %Target% --url https://repo.anaconda.com/miniconda/%Target%
+    mkdir %CondaDir%
+    echo Installing Miniconda into %CondaDir%\...
+    START /wait "" %Target% /S /D=%CondaDir%
+    DEL %Target%
+)
+
+SET CondaExecPath=conda\condabin\conda.bat
+SET CondaEnvsDir=.conda
+SET CondaEnvDir=%CondaEnvsDir%\iseg
+
+echo Creating conda environment...
+echo --------------------------------------------------------------------------------
+CALL %CondaExecPath% config --set remote_max_retries 3
+CALL %CondaExecPath% install -y -c conda-forge mamba
+CALL %CondaDir%\condabin\mamba.bat env create -p %CondaEnvsDir%\iseg -f environment.yml python=3.10
+
+echo Installation complete.
+
+PAUSE
