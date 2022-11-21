3f6b616c3824656c36371f0947606c875f404d97
wigginno <77313083+wigginno@users.noreply.github.com>
2022-11-21 14:11:25 -0500

    Download checkpoint file in windows install script

--- install_scripts/iseg-windows-installer.bat
+++ install_scripts/iseg-windows-installer.bat
@@ -23,9 +23,14 @@ SET CondaEnvDir=%CondaEnvsDir%\iseg
 
 echo Creating conda environment...
 echo --------------------------------------------------------------------------------
+mkdir %CondaEnvsDir%
 CALL %CondaExecPath% config --set remote_max_retries 3
 CALL %CondaExecPath% install -y -c conda-forge mamba
-CALL %CondaDir%\condabin\mamba.bat env create -p %CondaEnvsDir%\iseg -f environment.yml python=3.10
+CALL %CondaDir%\condabin\mamba.bat env create -p %CondaEnvsDir%\iseg -f ..\environment.yml python=3.10
+
+echo "Downloading model checkpoint file..."
+DEL ..\checkpoints\last_checkpoint.pth
+curl --output ..\checkpoints\last_checkpoint.pth https://media.githubusercontent.com/media/fogg-lab/interactive-segmentation/distribution/checkpoints/last_checkpoint.pth
 
 echo Installation complete.
 
